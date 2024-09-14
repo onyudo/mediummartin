af369307a5a7d087b5d679a42643590b1cda336e
“Martin <martin@forceandform.com>
2024-09-14 15:36:39 -0400

    updated src/save.js

--- src/save.js
+++ src/save.js
@@ -22,3 +22,9 @@ export default function save() {
 		</p>
 	);
 }
+
+import { useBlockProps } from '@wordpress/block-editor';
+export default function save( { attributes } ) {
+	const blockProps = useBlockProps.save();
+	return <div { ...blockProps }>{ attributes.message }</div>;
+}
